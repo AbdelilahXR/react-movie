98643101c46868165994eddac9704b61d558a266
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-06-09 13:11:00 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react' 
+import './App.css' 
+
+const App = () => {
+
+  return (
+      <main>
+          <div className='pattern'/>
+             <div className='wrapper'>
+                <header>
+                  <img src="./hero.png" alt="Hero Banner" />
+                  <h1>Find <span className='text-gradient'>Movies</span>You'il Enjoy Without the Hassle</h1>
+                </header>
+                <p>search</p>   
+          </div>
+
+         
+      </main>
+  )
+}
+
+export default App
